89052232a5cbeb8dee82297f9833de5c87670e0e
MariamGulordava <marimarigulo999@gmail.com>
2024-03-31 16:07:15 +0400

    Modified the Id field for the Users. Removed userId field.

--- db/config.ts
+++ db/config.ts
@@ -2,8 +2,7 @@ import { defineDb, defineTable, column } from 'astro:db';
 
 const Users = defineTable({
   columns: {
-    id: column.number({ primaryKey: true }),
-    userId: column.text(),
+    id: column.text({ primaryKey: true }),
     username: column.text(),
     hashed_password: column.text(),
     phone_number: column.text(),
